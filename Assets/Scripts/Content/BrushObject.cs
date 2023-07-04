e33e8cfb4fb738b0e5fd35dd10053682b55a35f4
Sebastian Keppler <sebastian.keppler@htw-berlin.de>
2023-07-04 18:57:39 +0200

    Added OpenXR Plugin

---
+++ Assets/Scripts/Content/BrushObject.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrushObject : ScriptableObject
+{
+    [SerializeField] private Color color = Color.white;
+    [SerializeField] private float size = 0.1f;
+    
+}
